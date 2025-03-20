bc1d598d80320c53d5e073b3655f29427464a7c3
Fabiano Valmorbida Marçal Pessôa <fabiano@fabianopessoa.com>
2025-03-20 21:46:59 +0000

    fix: Mudar para CDN do jsDelivr e adicionar headers CORS

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Adicionar headers CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 // Servir arquivos estáticos com cache-control
 app.use(express.static('.', {
     maxAge: '1y',
